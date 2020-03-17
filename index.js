05c2a0e15e41a8ea7aa0fcbcfb612b3518dc8338
Aravind <40268272+GeekRoot999@users.noreply.github.com>
2020-03-18 00:46:40 +0530

    added delete button to new Task on creation. on delete the task gets appended to a new unordered list. comparing the the task with already created task. on restore the task gets appended to the main unordered list.

--- index.js
+++ index.js
@@ -1,21 +1,101 @@
 var inputText = document.getElementById("myInput");
 var addTask = document.getElementsByClassName("addBtn")[0];
 var uol = document.getElementById("myUL");
-var listTask = document.getElementsByClassName("task-items");
-console.log(listTask);
+var TaskItems = document.getElementsByClassName("task-items");
+var deleteBtn =  document.createElement("button");
+var listOfItems = [
+    { id : 1, name: "task 1"}, 
+    { id : 2, name: "task 2"},
+    { id : 3, name: "task 3"},
+    { id : 4, name: "task 4"}
+ ]
 
 const newTask = () => {
-    var listTask = document.createElement("li");
-    listTask.appendChild(document.createTextNode(inputText.value));
-    uol.appendChild(listTask);
-    inputText.value = " ";
+    const Compare = compareTask();
+    if(Compare === false){
+        var span = document.createElement("span");
+        var deleteBtn =  document.createElement("button");
+        deleteBtn.innerText = "delete";
+        span.appendChild(deleteBtn);
+        var listTask = document.createElement("li");
+        listTask.appendChild(document.createTextNode(inputText.value));
+        listTask.addEventListener("click", checked);
+        listTask.appendChild(span);
+        deleteBtn.addEventListener("click", function(){
+            uol.removeChild(listTask);
+            addToAnotherUl(listTask.innerText);
+        })
+        listTask.classList.add("task-items");
+        uol.appendChild(listTask);
+        inputText.value = "";
+    }
+    else{
+        alert("Task already Exist");
+    }
 }
 
+const compareTask = () => {
+    let isFound = false;
+    var currentList = document.querySelectorAll("li");
+    for(let i = 0; i < currentList.length; i++){
+        const listTaskValue = currentList[i].innerHTML.split('<span>')[0];
+        // console.log("comparing", listTaskValue, "input", inputText.value);
+        if(listTaskValue.toLowerCase() == inputText.value.toLowerCase()){
+            isFound = true;
+            break;
+        }
+    }
+    return isFound;
+}
 
+// const newTask = () => {
+//     var currentList = document.querySelectorAll("li");
+//     for(let i = 0; i < currentList.length; i++){
+//         const listTaskValue = currentList[i].innerHTML.split('<span>')[0];
+//         if(listTaskValue.toLowerCase() == inputText.value.toLowerCase()){
+//             alert("task alreay exist");
+//             return;
+//         }
+//     }
+//     var span = document.createElement("span");
+//     var deleteBtn =  document.createElement("button");
+//     deleteBtn.innerText = "delete";
+//     span.appendChild(deleteBtn);
+//     var listTask = document.createElement("li");
+//     listTask.appendChild(document.createTextNode(inputText.value));
+//     listTask.addEventListener("click", checked);
+//     listTask.appendChild(span);
+//     deleteBtn.addEventListener("click", function(){
+//         uol.removeChild(listTask);
+//     })
+//     listTask.classList.add("task-items");
+//     uol.appendChild(listTask);
+//     inputText.value = "";
+// }
+
+var listTask = document.querySelectorAll("li");
+console.log(listTask);
+for(let i = 0; i < listTask.length; i++){
+    listTask[i].addEventListener("click", function(){
+        event.target.classList.toggle("checked");
+    });
+    var span = document.createElement("span");
+    var deleteBtn =  document.createElement("button");
+    deleteBtn.innerText = "delete";
+    span.appendChild(deleteBtn);
+    listTask[i].appendChild(span);
+    deleteBtn.addEventListener("click", function(){
+        uol.removeChild(listTask[i]);
+        addToAnotherUl(listTask[i].innerText);
+    })
+}
+
+const checked = (event) => {
+    event.target.classList.toggle("checked");
+}
 
 const onPressEnter = (e) => {
     if (e.keyCode === 13) {
-        console.log("hello");
         if (inputText.value.length > 0) {
             newTask();
         }
@@ -37,3 +117,30 @@ const onPressAdd = () => {
 addTask.addEventListener("click", onPressAdd);
 
 inputText.addEventListener("keypress", onPressEnter);
+
+
+function addToAnotherUl(deletedValue) {
+    var deleteul = document.createElement("ul");
+    var deletelist = document.createElement("li");
+    deletelist.innerText = deletedValue;
+    deleteul.appendChild(deletelist);
+    document.getElementsByClassName("delete-header")[0].appendChild(deleteul);
+    var restoreBtn = document.createElement("button");
+    var restore = document.createTextNode("restore");
+    restoreBtn.appendChild(restore);
+    deletelist.appendChild(restoreBtn);
+    restoreBtn.addEventListener("click", function(){
+        deleteul.removeChild(deletelist);
+        uol.appendChild(deletelist);
+        deletelist.removeChild(restoreBtn);
+        var span = document.createElement("span");
+        var deleteBtn =  document.createElement("button");
+        deleteBtn.innerText = "delete";
+        span.appendChild(deleteBtn);
+        deletelist.appendChild(deleteBtn);
+        deleteBtn.addEventListener("click", function(){
+            uol.removeChild(deletelist);
+            addToAnotherUl(deletelist.innerText);
+        })
+    })
+}
